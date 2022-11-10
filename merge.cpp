45eb72249b26102cddbeab3ac7809c46b50af2f4
Cauã Gomes <109199184+cauanzinhooo@users.noreply.github.com>
2022-11-09 22:05:08 -0300

    Update merge.cpp

---
+++ merge.cpp
@@ -0,0 +1,99 @@
+MERGE SORT COMPARADO AOS OUTROS ALGORITMOS É O MAIS COMPLEXO EM QUESTÃO DE LÓGICA
+USA O METODO POR ORDENAÇÃO QUE FUNCIONA DA SEGUINTE FORMA:
+
+MERGE SORT DIVIDE O VETOR NO MEIO
+ABRINDO PRA ESQUERDA
+E ACESSANDO OS SUBVETORES 
+E REALIZA INTERCALAÇÕES NOS SUBVETORES
+DEPOIS REALIZA O MESMO PROCESSO COM A METADE DO VETOR DA PARTE DA DIREITA.
+
+C EXEMPLE:
+
+intercala (int p, int q, int r, int v[]) 
+{
+   int *w;  // 1
+   w = malloc ((r-p) * sizeof (int));  // 2
+   int i = p, j = q;  // 3
+   int k = 0;  // 4
+
+   while (i < q && j < r) {  // 5
+      if (v[i] <= v[j])  w[k++] = v[i++];  // 6
+      else  w[k++] = v[j++];  // 7
+   }  // 8
+   while (i < q)  w[k++] = v[i++];  // 9
+   while (j < r)  w[k++] = v[j++];  // 10
+   for (i = p; i < r; ++i)  v[i] = w[i-p];  // 11
+   free (w);  // 12
+}
+
+
+
+////////////////////
+C++ EXEMPLE
+void swapping(int &a, int &b) {     //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void merge(int *array, int l, int m, int r) {
+   int i, j, k, nl, nr;
+   //size of left and right sub-arrays
+   nl = m-l+1; nr = r-m;
+   int larr[nl], rarr[nr];
+   //fill left and right sub-arrays
+   for(i = 0; i<nl; i++)
+      larr[i] = array[l+i];
+   for(j = 0; j<nr; j++)
+      rarr[j] = array[m+1+j];
+   i = 0; j = 0; k = l;
+   //marge temp arrays to real array
+   while(i < nl && j<nr) {
+      if(larr[i] <= rarr[j]) {
+         array[k] = larr[i];
+         i++;
+      }else{
+         array[k] = rarr[j];
+         j++;
+      }
+      k++;
+   }
+   while(i<nl) {       //extra element in left array
+      array[k] = larr[i];
+      i++; k++;
+   }
+   while(j<nr) {     //extra element in right array
+      array[k] = rarr[j];
+      j++; k++;
+   }
+}
+void mergeSort(int *array, int l, int r) {
+   int m;
+   if(l < r) {
+      int m = l+(r-l)/2;
+      // Sort first and second arrays
+      mergeSort(array, l, m);
+      mergeSort(array, m+1, r);
+      merge(array, l, m, r);
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];     //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   mergeSort(arr, 0, n-1);     //(n-1) for last index
+   cout << "Array after Sorting: ";
+   display(arr, n);
+}
